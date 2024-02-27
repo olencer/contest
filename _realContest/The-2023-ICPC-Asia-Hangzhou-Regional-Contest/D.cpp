36bedc548d4f3fa93af43d040ed9e975ef9a4ba6
olencer <jing255629@gmail.com>
2024-02-27 16:19:02 +0800

    Add(_realContest): The 2023 ICPC Asia Hangzhou Regional Contest.

---
+++ _realContest/The-2023-ICPC-Asia-Hangzhou-Regional-Contest/D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void solve()
+{
+
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    while (t--) { solve(); }
+}
