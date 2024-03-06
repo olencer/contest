22b1f758c178c1cc5b50799217bbe22d52038926
olencer <jing255629@gmail.com>
2024-03-06 10:40:28 +0800

    Archive(_executable, _todo): Self solved problems in Codeforces Round 932 (Div. 2).

--- _todo/Codeforces/Codeforces-Round-932/A.cpp
+++ _todo/Codeforces/Codeforces-Round-932/A.cpp
@@ -2,6 +2,21 @@
 
 void solve()
 {
+    int n;
+    std::string s;
+    std::cin >> n >> s;
+
+    int l = 0, r = s.size() - 1;
+    while (l < r && s[l] == s[r]) { ++l, --r; }
+
+    if(s[l] <= s[r]) {
+        std::cout << s << '\n';
+    } else {
+        std::reverse(s.begin(), s.end());
+        std::cout << s;
+        std::reverse(s.begin(), s.end());
+        std::cout << s << '\n';
+    }
 
 }
 
