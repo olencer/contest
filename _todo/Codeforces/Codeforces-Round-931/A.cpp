f238635bbdfb604def94e311efc6d90d536d8676
olencer <jing255629@gmail.com>
2024-03-06 13:16:08 +0800

    Archive(_executable, _todo): Self solved problems in Codeforces Round 931 (Div. 2)

--- _todo/Codeforces/Codeforces-Round-931/A.cpp
+++ _todo/Codeforces/Codeforces-Round-931/A.cpp
@@ -2,7 +2,17 @@
 
 void solve()
 {
+    int n;
+    std::cin >> n;
 
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+    }
+    std::sort(a, a + n);
+
+    int res = 2 * (a[n - 1] + a[n - 2] - a[0] - a[1]);
+    std::cout << res << '\n';
 }
 
 int main()
